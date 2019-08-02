6f2cda9835e08dc8c72489e1acc833cebbc8a2b9
Bando4ka <bando4ka@mail.ru>
2019-08-02 18:03:51 +0300

    Added Homework #6

---
+++ hw6/hw6/src/app/selected-picture/selected.picture.component.spec.ts
@@ -0,0 +1,25 @@
+﻿import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SelectedPictureComponent } from './selected.picture.component';
+
+describe('MainPictureComponent', () => {
+  let component: SelectedPictureComponent;
+  let fixture: ComponentFixture<SelectedPictureComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SelectedPictureComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SelectedPictureComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
